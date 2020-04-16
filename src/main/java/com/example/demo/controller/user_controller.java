427dfe40e984068be90ec247447b31801e525a00
ThakshilaN <thakshila.lnbti@gamil.com>
2020-04-16 15:41:30 +0530

    user logging

--- src/main/java/com/example/demo/controller/user_controller.java
+++ src/main/java/com/example/demo/controller/user_controller.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,22 @@ public class user_controller {
         String user = "Thakshila";
 
         return user;
+    }
+
+    @GetMapping("/login/{user}/{password}")
+    public String
+    getLoging(@PathVariable("user")String user,
+              @PathVariable("passwrd") String password) {
+        String userName = "Thakshila";
+        String passWord = "password";
+        String message = "";
+
+        if(userName == user && passWord == password){
+            message ="Approved User";
+        } else {
+            message = "User Not Allowed";
+        }
+
+        return message;
     }
 }
